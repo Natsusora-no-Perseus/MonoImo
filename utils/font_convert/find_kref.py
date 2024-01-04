c9a0b587ac8e92b956b28492a863c9ad8db7a3bd
Natsusora-no-Perseus <89143712+Natsusora-no-Perseus@users.noreply.github.com>
2024-01-04 14:13:11 -0600

    Bug fixes in utils/find_kref.py

--- utils/font_convert/find_kref.py
+++ utils/font_convert/find_kref.py
@@ -1,3 +1,23 @@
+# find_kref.py
+# Finding the glyph affected by kerning the least in a font.
+# Part of MonoImo library's utility scripts.
+#
+# Only works for Python version >= 3.8.
+# For help, run `python3 find_kref.py -h`
+#
+# For more complete documentation, visit the repository below:
+# https://github.com/Natsusora-no-Perseus/MonoImo
+#
+# This script is free software: you can distribute it and/or modify it under the terms
+# of the GNU Lesser General Public License as published by the Free Software
+# Foundation, either version 2.0 or later.
+# This script is distributed in hope that it will be useful, but WITHOUT ANY WARRANTY;
+# without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR
+# PURPOSE. See the GNU LGPL License for more details.
+#
+# (C) 2024- , https://github.com/Natsusora-no-Perseus
+
+
 import sys, argparse
 from PIL import ImageFont
 from collections import namedtuple
@@ -34,6 +54,12 @@ parser.add_argument('-d', '--descending',
                     action='store_true',
                     help='ranks in descending order instead')
 
+parser.add_argument('-c', '--count',
+                    help='how many glyphs are displayed in the result',
+                    default=10,
+                    nargs=1,
+                    type=int)
+
 
 args = parser.parse_args()
 chr_lst = []
@@ -44,9 +70,10 @@ KernStats = namedtuple('KernStats',
                         'rmean', 'rmed', 'rstdev', 'rmax'])
 # Make it print better:
 setattr(KernStats, "__repr__", lambda self:\
-        f"chr {self.chr}: lmean={self.lmean:.2f}, lmed={self.lmed:.2f}, "\
-        f"lstdev={self.lstdev:.2f}, lmax={self.lmax:.2f}, rmean={self.rmean:.2f}, "\
-        f"rmed={self.rmed:.2f}, rstdev={self.rstdev:.2f}, rmax={self.rmax:.2f}")
+        f"\"{self.chr}\": lmean={self.lmean:.4f}, lmed={self.lmed:.4f}, "\
+        f"lstdev={self.lstdev:.4f}, lmax={self.lmax:.4f};\n     "\
+        f"rmean={self.rmean:.4f}, rmed={self.rmed:.4f}, rstdev={self.rstdev:.4f}, " \
+        f"rmax={self.rmax:.4f}")
 
 
 try:
@@ -57,10 +84,10 @@ except OSError:
 
 # takes in two characters and compute the sum of their kerning, left and right
 def get_kern(ch1, ch2, font):
-    l_kern_l = font.getlength(ch1 + ch2)
-    l_nkern_l = font.getlength(ch1 + ch2, features=['-kern'])
-    r_kern_l = font.getlength(ch2 + ch1)
-    r_nkern_l = font.getlength(ch2 + ch1, features=['-kern'])
+    l_kern_l = font.getlength(ch2 + ch1)
+    l_nkern_l = font.getlength(ch2 + ch1, features=['-kern'])
+    r_kern_l = font.getlength(ch1 + ch2)
+    r_nkern_l = font.getlength(ch1 + ch2, features=['-kern'])
     return (abs(l_kern_l - l_nkern_l), abs(r_kern_l - r_nkern_l))
 
 if len(args.include) % 2 == 1:
@@ -79,10 +106,12 @@ for elem in args.include: # Iterate all characters non-repetitively
         if chr(i) not in chr_lst:
             chr_lst.append(chr(i))
 
-for elem in chr_lst:
-    tmp_lst = [get_kern(str(elem), str(i), font) for i in range(0, len(chr_lst))] # List of kerning val relative to all characters in chr_lst
+for ch1 in chr_lst:
+    # List of kerning val relative to all characters in chr_lst
+    tmp_lst = \
+            [get_kern(str(ch1), str(ch2), font) for ch2 in chr_lst]
     l_lst, r_lst = [val[0] for val in tmp_lst], [val[1] for val in tmp_lst]
-    kern_lst.append(KernStats(elem,
+    kern_lst.append(KernStats(ch1,
                               mean(l_lst), median(l_lst), stdev(l_lst), max(l_lst),
                               mean(r_lst), median(r_lst), stdev(r_lst), max(r_lst)))
 
@@ -93,7 +122,7 @@ sortfunc_lst = \
         [lambda x: x.lmean, lambda x: x.lmed, lambda x: x.lstdev, lambda x: x.lmax,
          lambda x: x.rmean, lambda x: x.rmed, lambda x: x.rstdev, lambda x: x.rmax]
 
-printcnt = 10
+printcnt = min(args.count[0], len(kern_lst)) 
 for sortword in sortword_lst:
     if sortword in args.rankby:
         print('======= Sorting by ' + sortword + ': =======')
